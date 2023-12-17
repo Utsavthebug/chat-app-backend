225190b417ff7a741ef335bc564cd1ab98d05b72
Utsavthebug <neupaneutsav.2001@gmail.com>
2023-12-17 21:08:01 +0545

    socket server added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import app from "./app.js";
 import logger from "./configs/logger.config.js";
 import { Server } from "socket.io";
+import SocketServer from "./SocketServer.js";
 
 //env variables
 const PORT = process.env.PORT || 8000
@@ -42,6 +43,7 @@ const io = new Server(server,{
 
 io.on("connection",(socket)=>{
     logger.info("socket io connected succesfully.")
+    SocketServer(socket,io)
 })
 
 const exitHandler = ()=>{
